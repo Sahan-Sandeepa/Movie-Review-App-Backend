971f97532d5914e76400b41b58b5d0b1ea5b2579
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-07-05 22:10:23 +0530

    created movie related classes

---
+++ src/main/java/dev/sahan/movies/Movie.java
@@ -0,0 +1,41 @@
+package dev.sahan.movies;
+
+import java.util.List;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collation = "Movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+
+    @Id
+    private ObjectId id;
+
+    private String imdbid;
+
+    private String title;
+
+    private String releasDate;
+    
+    private String trilerLink;
+
+    private String poster;
+
+    private List<String> genres;
+
+    private List<String> backdrops;
+
+    @DocumentReference
+    //means of the reference to another collection
+    private List<Review> reviewIds;
+    
+}
---
+++ src/main/java/dev/sahan/movies/Review.java
@@ -0,0 +1,21 @@
+package dev.sahan.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collation = "Reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    
+    @Id
+    private ObjectId id;
+
+    private String body;
+}
