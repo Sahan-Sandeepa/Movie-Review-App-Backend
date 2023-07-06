dd84360f7a6462861caa3c7399b43d8a243716a5
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-07-06 12:37:56 +0530

    removed spelling mistake

--- src/main/java/dev/sahan/movies/Review.java
+++ src/main/java/dev/sahan/movies/Review.java
@@ -8,7 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Document(collation = "Reviews")
+@Document(collection = "Reviews")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
