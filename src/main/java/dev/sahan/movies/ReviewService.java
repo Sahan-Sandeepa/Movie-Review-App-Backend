c9366d187d6bb0d34b6b774b3d72c931e1a4c25f
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-07-13 19:42:07 +0530

    modified review service for delete method

--- src/main/java/dev/sahan/movies/ReviewService.java
+++ src/main/java/dev/sahan/movies/ReviewService.java
@@ -15,16 +15,14 @@ public class ReviewService {
     @Autowired
     private MongoTemplate mongoTemplate;
 
-    public Review createReview(String reviewBody, String imdbId){
-
-        Review review =  reviewRepository.insert(new Review(reviewBody));
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = reviewRepository.insert(new Review(reviewBody, imdbId));
 
         mongoTemplate.update(Movie.class)
-        .matching(Criteria.where("imdbId").is(imdbId))
-        .apply(new Update().push("reviewIds").value(review))
-        .first();
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
 
         return review;
     }
-
 }
