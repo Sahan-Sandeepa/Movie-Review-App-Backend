8b29766434b745d089eef2428b9bb76aedc8c413
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-07-13 19:41:50 +0530

    modified review repository for delete method

--- src/main/java/dev/sahan/movies/ReviewRepository.java
+++ src/main/java/dev/sahan/movies/ReviewRepository.java
@@ -5,8 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReviewRepository extends MongoRepository<Review, ObjectId>{
-    
-
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
 
 }
+
